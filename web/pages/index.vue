032cc744ee326df7487d32fd52356a87e22839fe
shinzenatt <alen.mukaca@gmail.com>
2024-02-25 19:40:12 +0100

    Made an under construction page

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -3,9 +3,28 @@
 </script>
 
 <template>
-  <NuxtWelcome/>
+  <v-main>
+    <v-container>
+      <v-card>
+        <v-img color="yellow"
+               src="https://clipart-library.com/new_gallery/403950_construction-tape-png.png"
+               width="100%"
+               aspect-ratio="16/9"
+               max-height="700"
+        />
+        <v-card-title> In the mean time take a look at these pages:</v-card-title>
+        <v-card-text class="buttons">
+          <v-btn color="blue" prepend-icon="mdi-linkedin" href="https://www.linkedin.com/in/alen-mukaca/"> LinkedIn </v-btn>
+          <v-btn color="black" prepend-icon="mdi-github" href="https://github.com/ShinzenATT"> Github </v-btn>
+          <v-btn color="gray" prepend-icon="mdi-account" href="https://cv.shinzle.net/"> CV </v-btn>
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </v-main>
 </template>
 
 <style scoped>
-
+  .buttons .v-btn {
+    margin: 5px;
+  }
 </style>
--- web/plugins/vuetify.ts
+++ web/plugins/vuetify.ts
@@ -3,10 +3,11 @@ import '@mdi/font/css/materialdesignicons.css'
 
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { md3 } from 'vuetify/blueprints'
 
 export default defineNuxtPlugin((app) => {
     const vuetify = createVuetify({
-        // ... your configuration
+        blueprint: md3
     })
     app.vueApp.use(vuetify)
 })
